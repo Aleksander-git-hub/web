2ce9bac1433887386f26d56c89bc3e70bcf659c5
achugun <aleksander.chugunoff@yandex.ru>
2021-02-11 14:46:37 +0300

    1.2

--- src/main/java/com/project/web/mapper/UserMapper.java
+++ src/main/java/com/project/web/mapper/UserMapper.java
@@ -3,12 +3,10 @@ package com.project.web.mapper;
 import com.project.web.dto.UserDto;
 import com.project.web.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-    @Mapping(target = "id", ignore = true)
     User toEntity(UserDto userDto);
 
     UserDto toDto(User user);
